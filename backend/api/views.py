94a2e8a9745fd0df8f1d1e256b556449f1f25363
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2024-09-30 21:16:28 +0400

    Use rest framework serializer

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,12 +4,14 @@ from rest_framework.response import Response
 
 
 from products.models import Product
+from products.serializers import ProductSerializer
 
 
 @api_view(["GET"])
 def api_home(request, *args, **kwargs):
-    model_data = Product.objects.all().order_by("?").first()
+    instance = Product.objects.all().order_by("?").first()
     data = {}
-    if model_data:
-        data = model_to_dict(model_data, fields=["id", "price"])
+    if instance:
+        # data = model_to_dict(model_data, fields=["id", "price"])
+        data = ProductSerializer(instance).data
     return Response(data)
--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -6,4 +6,4 @@ from .models import Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ["title", "content", "price", "sale_price", "get_discount"]
+        fields = ["title", "content", "price"]
