2415eaf2b0ede53db558796e1550da133c65ac29
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2024-10-02 23:54:31 +0400

    Modify the basic.py file

--- py_client/basic.py
+++ py_client/basic.py
@@ -3,7 +3,7 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/"
 
-response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello Vector"})
+response = requests.post(endpoint, json={"title": "Hello World"})
 # print(response.status_code)
 # print(response.text)
 print(response.json())
