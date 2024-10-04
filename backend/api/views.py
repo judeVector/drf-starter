c2010a91d99bb2e7c0943b1601ef7f5e973feeb9
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2024-10-04 13:52:08 +0400

    Update the api home function

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,14 +1,12 @@
-from django.forms import model_to_dict
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
-from products.models import Product
 from products.serializers import ProductSerializer
 
 
 @api_view(["POST"])
-def api_home(request, *args, **kwargs):
+def api_home(request):
     serializer = ProductSerializer(data=request.data)
     if serializer.is_valid(raise_exception=True):
         serializer.save()
