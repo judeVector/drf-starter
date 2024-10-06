648a4ebbb84c379a31553c544a5565c6bf9e0968
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2024-10-06 22:04:31 +0400

    Create views using mixins

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import ProductDetailAPIView, ProductCreateAPIView
+from .views import ProductDetailAPIView, ProductCreateAPIView, ProductMixinView
 
 urlpatterns = [
     path("<int:pk>/", ProductDetailAPIView.as_view()),
-    path("", ProductCreateAPIView.as_view()),
+    path("", ProductMixinView.as_view()),
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, mixins
 
 from .models import Product
 from .serializers import ProductSerializer
@@ -14,5 +14,22 @@ class ProductCreateAPIView(generics.ListCreateAPIView):
     serializer_class = ProductSerializer
 
     def perform_create(self, serializer):
-        print(serializer)
-        serializer.save()
+        title = serializer.validated_data.get("title")
+        content = serializer.validated_data.get("content") or None
+
+        if content is None:
+            content = title
+            serializer.save(content=content)
+
+
+class ProductMixinView(
+    mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView
+):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
