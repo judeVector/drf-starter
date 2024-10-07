b5b569b2ef0112af329ce6ecf75cd81891ff07fc
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2024-10-07 15:39:56 +0400

    Add permission classes

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from .views import ProductDetailAPIView, ProductCreateAPIView, ProductMixinView
+from .views import (
+    ProductDetailAPIView,
+    ProductCreateAPIView,
+    ProductMixinView,
+)
 
 urlpatterns = [
-    path("<int:pk>/", ProductDetailAPIView.as_view()),
-    path("", ProductMixinView.as_view()),
+    path("<int:pk>/", ProductMixinView.as_view()),
+    path("", ProductCreateAPIView.as_view()),
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, mixins
+from rest_framework import generics, mixins, permissions
+
 
 from .models import Product
 from .serializers import ProductSerializer
@@ -7,8 +8,10 @@ from .serializers import ProductSerializer
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
+# Using generics in class-based views
 class ProductCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
@@ -22,14 +25,34 @@ class ProductCreateAPIView(generics.ListCreateAPIView):
             serializer.save(content=content)
 
 
+# Using mixins and generics in class-based views
 class ProductMixinView(
-    mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.DestroyModelMixin,
+    generics.GenericAPIView,
 ):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    lookup_field = "pk"
 
     def get(self, request, *args, **kwargs):
+        pk = kwargs.get("pk")
+        if pk is not None:
+            return self.retrieve(request, *args, **kwargs)
         return self.list(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        title = serializer.validated_data.get("title")
+        content = serializer.validated_data.get("content") or None
+
+        if content is None:
+            content = title
+            serializer.save(content=content)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
