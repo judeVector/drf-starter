d9345dfa8c1ca0e2c7b33ab2263fec0279372ddd
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2024-10-13 00:23:15 +0400

    Add a doctring comment to the tokenauthentication class

--- backend/api/authentication.py
+++ backend/api/authentication.py
@@ -2,4 +2,8 @@ from rest_framework.authentication import TokenAuthentication as BaseTokenAuth
 
 
 class TokenAuthentication(BaseTokenAuth):
+    """
+    Custom Authentication adding the keyword Bearer
+    """
+
     keyword = "Bearer"
