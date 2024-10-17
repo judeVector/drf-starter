901b277fea4db27a9c05211966621ebe3191a6f7
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2024-10-17 22:42:53 +0400

    Update the urls to be use simplejwt

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("api.urls")),
+    path("", include("api.urls")),
     path("api/articles/", include("articles.urls")),
     path("api/products/", include("products.urls")),
     path("api/v2/", include("core.routers")),
